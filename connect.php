c2f72a75c9f28cc7db13e739a4e941cec75b2b64
Satyarth Agrahari <satyarthagrahari@gmail.com>
2018-04-07 19:02:02 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 
 $db_host = 'localhost'; 
 $db_user = 'root'; 
-$db_pass = 'superman1234'; 
+$db_pass = ''; 
 $db_name = 'library'; 
 
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
